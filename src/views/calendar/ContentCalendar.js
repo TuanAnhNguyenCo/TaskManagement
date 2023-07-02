c5eedf1d709d6eb8f3a11b7e5f8261aefa100587
Nguyen Co Tuan Anh <coanh2710@gmail.com>
2023-07-02 23:00:43 +0700

    add cursor to progressbar

--- src/views/calendar/ContentCalendar.js
+++ src/views/calendar/ContentCalendar.js
@@ -128,7 +128,7 @@ const ContentCalendar = (props) => {
                                     }>
 
 
-                                        <ProgressBar>
+                                        <ProgressBar style={{cursor:'pointer'}}>
 
 
 
